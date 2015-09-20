1317b149016cf4d94512fa92cd39b2825da2308e
tnrich <trich1230@gmail.com>
2015-09-20 14:13:40 -0700

    adding production build config

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,3 +1,4 @@
+var webpack = require("webpack");
 module.exports = {
     entry: "./app/App.js",
     output: {
@@ -16,6 +17,14 @@ module.exports = {
             }
         ]
     },
+    plugins: [
+        //tnr: this plugin sets the NODE_ENV variable to production which deactivates both react proptypes and api-check!
+        new webpack.DefinePlugin({ 
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        })
+    ],
     resolve: {
         // you can now require('file') instead of require('file.coffee')
         extensions: ['', '.js', '.json', '.coffee']
