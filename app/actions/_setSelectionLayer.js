55eb9b4cac101abab60f176fd92c6ed348f17b12
tnrich <trich1230@gmail.com>
2015-09-26 09:36:28 -0700

    adding file

---
+++ app/actions/_setSelectionLayer.js
@@ -0,0 +1,55 @@
+var setCaretPosition = require('./setCaretPosition');
+var areNonNegativeIntegers = require('validate.io-nonnegative-integer-array');
+
+/**
+ * sets the selection layer on a plasmid
+ * @param  {object} newSelectionLayer {start: int, end: int, [cursorAtEnd: boolean]}
+ * @return {undefined}                   
+ */
+module.exports = function _setSelectionLayer (newSelectionLayer) {
+    var getRidOfCursor;
+    if (!newSelectionLayer || typeof newSelectionLayer !== 'object') {
+        //no selection layer passed, so cancel it
+        newSelectionLayer = {
+            start: -1,
+            end: -1,
+            selected: false,
+            cursorAtEnd: true
+        };
+    } else {
+        var start = newSelectionLayer.start;
+        // var selected = newSelectionLayer.selected;
+        var end = newSelectionLayer.end;
+        var cursorAtEnd;
+        if (newSelectionLayer.cursorAtEnd || typeof newSelectionLayer.cursorAtEnd === 'undefined') {
+            //if cursorAtEnd is passed as true, or if no cursorAtEnd is passed, auto set it to true
+            cursorAtEnd = true;
+        } else {
+            //if cursorAtEnd is explicitely passed as false, set it to false
+            cursorAtEnd = false;
+        }
+        if (areNonNegativeIntegers([start, end])) {
+            //valid selection layer passed, so set it.
+            newSelectionLayer = {
+                start: start,
+                end: end,
+                selected: true,
+                cursorAtEnd: cursorAtEnd 
+            };
+            getRidOfCursor = true;
+        } else {
+            //invalid selection layer passed, so cancel it
+            newSelectionLayer = {
+                start: -1,
+                end: -1,
+                selected: false,
+                cursorAtEnd: true
+            };
+        }
+    }
+    // if (!deepEqual(selectionLayer, newSelectionLayer)) { //tnrtodo come back here and reinstate this check once baobab has been fixed
+    if (getRidOfCursor) {
+        setCaretPosition(-1);
+    }
+    return({newSelectionLayer, newCaretPosition});
+};
