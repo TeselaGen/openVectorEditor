99f14a13fb039e6ddf1a8fc3b8aa8f0bb608dc47
tnrich <trich1230@gmail.com>
2015-10-10 01:56:29 -0700

    updating to use react dom

--- app/signals.js
+++ app/signals.js
@@ -7,21 +7,21 @@ reqContext.keys().forEach(function(key) {
 //add all the signals to the cerebral controller here
 export default function registerSignals(controller) {
     //tnr:  WORKING: 
-    controller.signal('copySelection', a.getSelectionLayer, a.copySelection);
-    controller.signal('selectAll', a.selectAll, a.setSelectionLayer);
-    controller.signal('sequenceDataInserted', 
+    controller.signal('copySelection', [a.getSelectionLayer, a.copySelection]);
+    controller.signal('selectAll', [a.selectAll, a.setSelectionLayer]);
+    controller.signal('sequenceDataInserted', [
         a.getSelectionLayer, {
             success: [a.deleteSequence]
-        }, a.getCaretPosition, a.insertSequenceData); 
-    controller.signal('setCutsiteLabelSelection', a.setCutsiteLabelSelection);
-    controller.signal('setCaretPosition', a.setCaretPosition);
-    controller.signal('editorClicked', a.setCaretPosition, a.setSelectionLayer);
+        }, a.getCaretPosition, a.insertSequenceData]); 
+    controller.signal('setCutsiteLabelSelection', [a.setCutsiteLabelSelection]);
+    controller.signal('setCaretPosition', [a.setCaretPosition]);
+    controller.signal('editorClicked', [a.setCaretPosition, a.setSelectionLayer]);
     //tnr: MOSTLY WORKING: 
-    controller.signal('backspacePressed', a.getSelectionLayer, {
+    controller.signal('backspacePressed', [a.getSelectionLayer, {
         success: [a.deleteSequence],
         error: [a.getCaretPosition, a.prepDeleteOneBack, a.deleteSequence]
-    });
-    controller.signal('caretMoved', 
+    }]);
+    controller.signal('caretMoved', [
         a.getData('selectionLayer'),
         a.getCaretPosition,
         // a.getSequenceLength,
@@ -30,17 +30,17 @@ export default function registerSignals(controller) {
             shiftHeld: [a.moveCaretShiftHeld, a.setSelectionLayer],
             noShift: [a.moveCaretNoShift, a.clearSelectionLayer]
         },
-    );
+    ]);
 
     //tnr: NOT YET WORKING:
     //higher priority
-    controller.signal('pasteSequenceString', a.pasteSequenceString);
-    controller.signal('setSelectionLayer', a.setSelectionLayer);
-    controller.signal('toggleAnnotationDisplay', a.setCaretPosition);
+    controller.signal('pasteSequenceString', [a.pasteSequenceString]);
+    controller.signal('setSelectionLayer', [a.setSelectionLayer]);
+    controller.signal('toggleAnnotationDisplay', [a.setCaretPosition]);
 
     //lower priority
-    controller.signal('addAnnotations', a.addAnnotations);
-    controller.signal('jumpToRow', a.jumpToRow);
+    controller.signal('addAnnotations', [a.addAnnotations]);
+    controller.signal('jumpToRow', [a.jumpToRow]);
 
 
 }
