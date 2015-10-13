db2f5ecbb62587782fe661ee148da7016d53f012
Jacob Coble <jcoble@lbl.gov>
2015-10-13 16:06:47 -0700

    recursively load all tests

--- test/registerTests.js
+++ test/registerTests.js
@@ -1,12 +1,3 @@
-var reqContext = require.context('../app/', false, /.*\.test\.js/);
+var reqContext = require.context('../app/', true, /.*\.test\.js/);
 var a = {};
-reqContext.keys().forEach(function(key) {
-    a[key.substring(2)] = reqContext(key)
-});
-var reqContext = require.context('../app/actions', false, /.*\.test\.js/);
-var a = {};
-reqContext.keys().forEach(function(key) {
-    a[key.substring(2)] = reqContext(key)
-});
-
-console.log('hello moto')
+reqContext.keys().forEach(reqContext);
