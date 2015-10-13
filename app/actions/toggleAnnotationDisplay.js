1e9b7a80cc94413465b9ec200dd8cf05a7560098
Sarah A LaFrance <salafrance@lbl.gov>
2015-10-13 11:39:11 -0700

    tidying up and removing verbose console logs

--- app/actions/toggleAnnotationDisplay.js
+++ app/actions/toggleAnnotationDisplay.js
@@ -3,13 +3,13 @@ var capitalize = require('capitalize');
 export default function toggleAnnotationDisplay(annotationType, tree, output) {
     ac.throw(ac.annotationType, annotationType);
     var capitalizedType = capitalize(annotationType);
-    console.log(capitalizedType);
+    // console.log(capitalizedType);
 
     if (tree.get('show' + capitalizedType)) {
         tree.set('show' + capitalizedType, false);
-        console.log("set false");
+        // console.log("set false");
     } else {
         tree.set('show' + capitalizedType, true);
-        console.log("set true");
+        // console.log("set true");
     }
 }
