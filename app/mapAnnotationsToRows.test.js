646c11c8904a79caf6eb3b0a3b823480526e1c5a
tnrich <trich1230@gmail.com>
2015-10-24 20:09:38 -0700

    adding skipped, failing mapAnnotationsToRows tests

--- app/mapAnnotationsToRows.test.js
+++ app/mapAnnotationsToRows.test.js
@@ -51,6 +51,87 @@ describe('mapAnnotationsToRows', function() {
             }]
         });
     });
+    it.skip('correctly calculates y-offset for annotation split by origin in different order', function() {
+        var annotation1 = {
+            start: 7,
+            end: 9,
+            id: 'a'
+        };
+        var annotation2 = {
+            start: 5,
+            end: 3,
+            id: 'b'
+        };
+        var annotations = [annotation1, annotation2];
+        var sequenceLength = 10;
+        var bpsPerRow = 10;
+        var annotationsToRowsMap = mapAnnotationsToRows(annotations, sequenceLength, bpsPerRow);
+        expect(annotationsToRowsMap).to.deep.equal({
+            0: [{
+                start: 7,
+                end: 9,
+                id: annotation2.id,
+                yOffset: 1,
+                annotation: annotation2,
+                enclosingRangeType: "beginningAndEnd"
+            },{
+                annotation: annotation1,
+                start: 5,
+                end: 9,
+                id: annotation1.id,
+                yOffset: 0,
+                enclosingRangeType: "beginning"
+            },{
+                annotation: annotation1,
+                start: 0,
+                end: 3,
+                id: annotation1.id,
+                yOffset: 0,
+                enclosingRangeType: "end"
+            },]
+        });
+    });
+    it.skip('correctly calculates y-offset for annotation split by origin', function() {
+        var annotation1 = {
+            start: 5,
+            end: 3,
+            id: 'a'
+        };
+        var annotation2 = {
+            start: 7,
+            end: 9,
+            id: 'b'
+        };
+        var annotations = [annotation1, annotation2];
+        var sequenceLength = 10;
+        var bpsPerRow = 10;
+        var annotationsToRowsMap = mapAnnotationsToRows(annotations, sequenceLength, bpsPerRow);
+        expect(annotationsToRowsMap).to.deep.equal({
+            0: [{
+                annotation: annotation1,
+                start: 5,
+                end: 9,
+                id: annotation1.id,
+                yOffset: 0,
+                enclosingRangeType: "beginning"
+            },{
+                annotation: annotation1,
+                start: 0,
+                end: 3,
+                id: annotation1.id,
+                yOffset: 0,
+                enclosingRangeType: "end"
+            },{
+                start: 7,
+                end: 9,
+                id: annotation2.id,
+                yOffset: 1,
+                annotation: annotation2,
+                enclosingRangeType: "beginningAndEnd"
+            }]
+        });
+    });
+    
     it('maps single annotation to rows correctly', function() {
         var annotation1 = {
             start: 0,
