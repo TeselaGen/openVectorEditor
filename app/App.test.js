d1ddff0e2455d0fedee053fe8c82ff3ce9a9e6cb
tnrich <trich1230@gmail.com>
2015-10-27 12:57:35 -0700

    updating smoke test

--- app/App.test.js
+++ app/App.test.js
@@ -1,1 +1,17 @@
-require('./App.js');
+import ReactDOM from 'react-dom';
+import App from './App'
+
+var options = {
+	actions: {
+
+	},
+	state: {
+
+	}
+}
+
+var {Editor, controller} = App(options);
+
+const DOMNodeToRenderTo = document.createElement('div');
+document.body.appendChild(DOMNodeToRenderTo);
+ReactDOM.render(Editor, DOMNodeToRenderTo);
