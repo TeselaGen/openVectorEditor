a2ed9a7ca55bf0046b4982567daef2327197fd55
tnrich <trich1230@gmail.com>
2015-10-28 11:20:29 -0700

    suppressing webpack output if everything runs smoothly

--- devServer.js
+++ devServer.js
@@ -7,7 +7,7 @@ var app = express();
 var compiler = webpack(config);
 
 app.use(require('webpack-dev-middleware')(compiler, {
-  // noInfo: true,
+  noInfo: true,
   publicPath: config.output.publicPath
 }));
 
