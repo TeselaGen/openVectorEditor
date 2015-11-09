c9ef76d896b323411908e29ccdbceb562c0ed29b
Shakeel Shafiq <shakeel.shafique@gmail.com>
2015-11-10 01:59:12 +0500

    dont need sequenceLength

--- app/actions/selectInverse.js
+++ app/actions/selectInverse.js
@@ -1,6 +1,5 @@
 export default function selectInverse(input, tree, output) {
     //compare the sequenceString being pasted in with what's already stored in the clipboard
-    var sequenceLength = tree.get(['sequenceLength']);
     var selectionLayer = tree.get(['selectionLayer']);
 
     output({
