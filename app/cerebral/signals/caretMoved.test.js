474ced234b239b2d8ef8d4740f6c273a977e8563
tnrich <trich1230@gmail.com>
2015-11-10 00:12:37 -0800

    updating the caretMoved tests

--- app/cerebral/signals/caretMoved.test.js
+++ app/cerebral/signals/caretMoved.test.js
@@ -5,26 +5,76 @@ var controller = require('../controller')({
             selected: false,
         },
         caretPosition: 1,
-        sequenceData: {sequence: 'atat'},
+        sequenceData: {
+            sequence: 'atat',
+            circular: true
+        },
         bpsPerRow: 2 //override the usual calc here
+        //seq looks like:
+        //at
+        //at
     }
 });
 
 var testSetup = require('./testSetup');
 
 describe('caretMoved', function() {
-    beforeEach(function() {
-        controller.recorder.start();
-    })
-    afterEach(function (argument) {
-        controller.recorder.reset()
-    })
-    it('should highlight when nothing is previously selected', function(done) {
+    it('moveCaretLeftOne should move the cursor left 1', function(done) {
         testSetup(controller, controller.signals.caretMoved, {
             type: 'moveCaretLeftOne',
             shiftHeld: false,
         }, function() {
             controller.get('caretPosition').should.equal(0);
+            done()
+        })
+    });
+    it('moveCaretLeftOne should move the cursor left 1 and around the sequence', function(done) {
+        controller.tree.set('caretPosition', 0);
+        testSetup(controller, controller.signals.caretMoved, {
+            type: 'moveCaretLeftOne',
+            shiftHeld: false,
+        }, function() {
+            controller.get('caretPosition').should.equal(3);
+            done()
+        })
+    });
+    it('moveCaretRightOne should move the cursor right 1', function(done) {
+        controller.tree.set('caretPosition', 1);
+        testSetup(controller, controller.signals.caretMoved, {
+            type: 'moveCaretRightOne',
+            shiftHeld: false,
+        }, function() {
+            controller.get('caretPosition').should.equal(2);
+            done()
+        })
+    });
+    it('moveCaretRightOne should move the cursor right 1 and around the sequence', function(done) {
+        controller.tree.set('caretPosition', 4);
+        testSetup(controller, controller.signals.caretMoved, {
+            type: 'moveCaretRightOne',
+            shiftHeld: false,
+        }, function() {
+            controller.get('caretPosition').should.equal(1);
+            done()
+        })
+    });
+    it('moveCaretUpARow should move the cursor up 2 places', function(done) {
+        controller.tree.set('caretPosition', 4);
+        testSetup(controller, controller.signals.caretMoved, {
+            type: 'moveCaretUpARow',
+            shiftHeld: false,
+        }, function() {
+            controller.get('caretPosition').should.equal(2);
+            done()
+        })
+    });
+    it('moveCaretUpARow should move the cursor up 2 places and around the sequence', function(done) {
+        controller.tree.set('caretPosition', 0);
+        testSetup(controller, controller.signals.caretMoved, {
+            type: 'moveCaretUpARow',
+            shiftHeld: false,
+        }, function() {
+            controller.get('caretPosition').should.equal(2);
             done()
         })
     });
