fd67edcbeecfe6f78f80d8ef00dc366228544ed8
tnrich <trich1230@gmail.com>
2015-11-16 23:18:13 -0800

    fix click events

--- app/cerebral/actions/updateSelectionShiftClick.js
+++ app/cerebral/actions/updateSelectionShiftClick.js
@@ -1,14 +1,14 @@
 var expandOrContractNonCircularRangeToPosition = require('ve-range-utils/expandOrContractNonCircularRangeToPosition');
 var expandOrContractCircularRangeToPosition = require('ve-range-utils/expandOrContractCircularRangeToPosition');
 var ac = require('ve-api-check');
-export default function updateSelectionShiftClick({updatedCaretPos, sequenceLength, caretPosition, selectionLayer}, tree, output) {
-    if (selectionLayer.start > selectionLayer.end) {
-        var {newRange, endMoved} = expandOrContractCircularRangeToPosition(selectionLayer, updatedCaretPos, sequenceLength);
+export default function updateSelectionShiftClick({nearestBP, sequenceLength, caretPosition, selectionLayer}, tree, output) {
+    if (true || selectionLayer.start > selectionLayer.end) {
+        var {newRange, endMoved} = expandOrContractCircularRangeToPosition(selectionLayer, nearestBP, sequenceLength);
         newRange.cursorAtEnd = endMoved;
         output({selectionLayer: newRange})
     } else {
         /*eslint-disable no-redeclare*/
-        var {newRange, endMoved} = expandOrContractNonCircularRangeToPosition(selectionLayer, updatedCaretPos);
+        var {newRange, endMoved} = expandOrContractNonCircularRangeToPosition(selectionLayer, nearestBP);
         /*eslint-enable no-redeclare*/
         newRange.cursorAtEnd = endMoved;
         output({selectionLayer: newRange})
