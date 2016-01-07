3206e52e6f1ab7a02b6164b6522a435f1ca2b555
Sarah A LaFrance <salafrance@lbl.gov>
2016-01-07 13:51:37 -0800

    checkshift borken

--- app/cerebral/actions/checkLayerIsSelected.js
+++ app/cerebral/actions/checkLayerIsSelected.js
@@ -1,6 +1,5 @@
 export default function checkLayerIsSelected(input, tree, output) {
     var selectionLayer = tree.get('selectionLayer');
-    // console.log(">>>>>> " + selectionLayer.selected);
 
     if (selectionLayer.selected) {
         output.selected();
--- app/cerebral/actions/checkShiftHeld.test.js
+++ app/cerebral/actions/checkShiftHeld.test.js
@@ -1,28 +1,37 @@
+import set from 'cerebral-addons/set';
+
 var checkShiftHeld = require('./checkShiftHeld');
+var controller = require('../controller')({
+    //instantiate some default val's here:
+    // state: {
+    //     shiftHeld: true
+    // }
+});
+
 describe('checkShiftHeld', function() {
     it('should call success when shiftHeld = true', function(done) {
-        checkShiftHeld({
-            shiftHeld: true
-        }, {}, {
+        set(['shiftHeld'], true), [
+        checkShiftHeld({}, controller.tree, {
             shiftHeld: function() {
                 done()
             },
             shiftNotHeld: function() {
                 throw new Error();
             }
-        });
+        })];
     });
+    // controller.reset();
+    // controller.tree.set('shiftHeld', false);
     it('should call error when shiftHeld = false', function(done) {
-        checkShiftHeld({
-            shiftHeld: false
-        }, {}, {
+        set(['shiftHeld'], false), [        
+        checkShiftHeld({}, controller.tree, {
             shiftHeld: function() {
                 throw new Error();
             },
             shiftNotHeld: function() {
                 done()
             }
-        });
+        })];
     });
 
 });
