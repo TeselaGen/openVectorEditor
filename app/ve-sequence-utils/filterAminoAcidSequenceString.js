b494a289a248fcb7a9319d6fc6242ad95064af42
Kyle Craft <kcraft@teselagen.com>
2016-02-10 11:48:18 -0800

    Added x as an acceptable Amino Acid character

--- app/ve-sequence-utils/filterAminoAcidSequenceString.js
+++ app/ve-sequence-utils/filterAminoAcidSequenceString.js
@@ -1,6 +1,6 @@
 var ac = require('ve-api-check');
 module.exports = function filterAminoAcidSequenceString(sequenceString) {
     ac.throw(ac.string, sequenceString);
-    return sequenceString.replace(/[^galmfwkqespvicyhrnd]/ig, '');
+    return sequenceString.replace(/[^gaxlmfwkqespvicyhrnd]/ig, '');
 }
 
