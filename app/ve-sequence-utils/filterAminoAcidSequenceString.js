94334f5feac3a9ec4e7b83b08e51e315d4259383
Thomas Rich <trich1230@gmail.com>
2016-02-10 16:22:43 -0800

    removing deprecated files

--- app/ve-sequence-utils/filterAminoAcidSequenceString.js
+++
@@ -1,6 +0,0 @@
-var ac = require('ve-api-check');
-module.exports = function filterAminoAcidSequenceString(sequenceString) {
-    ac.throw(ac.string, sequenceString);
-    return sequenceString.replace(/[^gaxlmfwkqespvicyhrnd]/ig, '');
-}
-
