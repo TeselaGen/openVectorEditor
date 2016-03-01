cf817395c33b0a72fca9c51a3a491a9ba1a3c13d
Jacob Coble <jcoble@lbl.gov>
2016-03-01 13:45:39 -0800

    require sequence data in RowView props

--- app/RowView/RowView.js
+++ app/RowView/RowView.js
@@ -5,9 +5,16 @@ import styles from './RowView.scss';
 
 import ResizeSensor from 'css-element-queries/src/ResizeSensor';
 
+@propTypes({
+    sequenceData: PropTypes.object.isRequired
+})
 export default class RowView extends React.Component {
 
     render() {
+        var {
+            sequenceData
+        } = this.props;
+
         return (
             <div ref={'rowView'}
                  className={styles.rowView}
