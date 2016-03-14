a05ad3fd8bc6c241fb6b9ce9f1eceb2c2ea8ac74
Sarah A LaFrance <salafrance@lbl.gov>
2016-03-14 10:24:02 -0700

    starting to work on load from file leveraging bio parsers

--- app/cerebral/actions/loadFromFile.js
+++ app/cerebral/actions/loadFromFile.js
@@ -1,19 +1,35 @@
 // load info in from a genbank, sbol, or fasta file
 // uses npmjs library bio-parsers
 // uses superagent to post the file back to node server
+import request from 'superagent/lib/client';
+
+var query = location.search;
+var cookie = document.cookie;
+var id = query.match(/entryId=[\d]+/) + "";
+id = id.replace(/entryId=/, "");
+var sid = cookie.match(/sessionId=%22[0-9a-z\-]+%22/) + "";
+sid = sid.replace(/sessionId=|%22/g, "");
 
 export default function saveToFile({input, state, output}) {
     var seqFileParser = require('bio-parsers/parsers/anyToJSON');
 
     console.log("did it. :3");
 
-    // if(fileExt.match("sbol|gb|fasta")
-    //     && id
-    //     && sid) 
-    // {
-    //     window.open('rest/file/' + id + '/sequence/' + fileExt + '?sid=' + sid)
-    // } else {
-    //     console.log("something went wrong, unable to find file");
-    // }
+    if(id && sid) 
+    {
+        request
+            .post('rest/parts/sequence')
+            .set('X-ICE-Authentication-sessionId', sid)
+            .set('Content-Type', 'application/json')
+            .send(/* results of seqfileparser? or is that on node server*/)
+            .end(function(err, result) {
+                if(err) {
+                    console.log("unable to load file, something went wrong: " + err)
+                }
+            }
+        );
+    } else {
+        console.log("something went wrong, unable to upload file");
+    }
 
 }
