a0401e10b5bbea9f6636cccef30db13b1c06d42f
Sarah A LaFrance <salafrance@lbl.gov>
2016-03-15 12:29:12 -0700

    removing bson ids, currently doesnt work

--- app/cerebral/actions/pasteSequenceString.js
+++ app/cerebral/actions/pasteSequenceString.js
@@ -1,4 +1,4 @@
-var ObjectID = require("bson-objectid");
+// var ObjectID = require("bson-objectid");
 var assign = require('lodash/object/assign');
 var filterSequenceString = require('ve-sequence-utils/filterSequenceString');
 
@@ -7,36 +7,29 @@ export default function pasteSequenceString({input, state, output}) {
     var cleanedUpClipboardData;
     var { sequenceString } = input;
 
-    function generateNewIdsForSequenceAnnotations(sequenceData) {
+    function removeFeatureIds(sequenceData) {
         return assign({}, sequenceData, {
-            features: generateNewIdsForAnnotations(sequenceData.features),
-            parts: generateNewIdsForAnnotations(sequenceData.parts)
+            features: removeIds(sequenceData.features)
         });
     }
 
-    function generateNewIdsForAnnotations(annotations) {
+    // // {{}} delete id instead of putting anything there, remove entirely
+    function removeIds(annotations) {
         return annotations.map(function(annotation) {
-            return assign({}, annotation, {
-                id: ObjectID().str
-            });
+            delete annotation._id;
         });
     }
-
-    // console.log("starting input string: " + sequenceString);
-    // console.log("starting clipboard data: " + clipboardData.sequence);
 
     if (clipboardData && clipboardData.sequence && clipboardData.sequence === sequenceString) {
         // handle clipboardData which was copied from within the app
-        // assign clipboardData annotations new ids
-        cleanedUpClipboardData = generateNewIdsForSequenceAnnotations(clipboardData);
-        // console.log(">>>> data was copied from inside the editor");
+        // remove ids from the copied features so the server can give them new ones
+        // cleanedUpClipboardData = removeFeatureIds(clipboardData);
+        cleanedUpClipboardData = clipboardData;
     } else {
         // clean up the sequence string coming from elsewhere so we can insert it
         cleanedUpClipboardData = filterSequenceString(sequenceString);
-        // console.log(">>>> data is from outside the editor");
     }
 
-    // console.log("clipboard data is clean: " + cleanedUpClipboardData);
     if(cleanedUpClipboardData.sequence) {
         output.success({'newSequenceData': cleanedUpClipboardData})
     } else {
