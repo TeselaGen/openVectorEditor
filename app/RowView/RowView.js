cbea10def427d1582e487a4be4f79f7beae6314c
salafrance <sea.car.flan@gmail.com>
2016-08-11 13:57:53 -0700

    patching in takara rowview, compiles

--- app/RowView/RowView.js
+++ app/RowView/RowView.js
@@ -1,179 +1,160 @@
-import React, {PropTypes} from 'react';
-import {Decorator as Cerebral} from 'cerebral-view-react';
+import some from 'lodash/collection/some'
+// import moveCaret from '../VectorInteractionWrapper/moveCaret'
+// import handleCaretMoved from '../VectorInteractionWrapper/handleCaretMoved'
+// import prepareRowData from './prepareRowData'
+import React from 'react';
+import Draggable from 'react-draggable'
+// import RowItem from './RowItem'
+// import ReactList from 'react-list';
+// import './style.scss';
 
-var Draggable = require('react-draggable');
-var RowItem = require('./RowItem.js');
-// var InfiniteScroller = require('react-variable-height-infinite-scroller');
+var defaultContainerWidth = 400
+var defaultCharWidth = 12
+var defaultMarginWidth = 10
+// var Combokeys = require("combokeys");
+// var combokeys;
 
-import styles from './RowView.scss';
+function noop() {
+}
 
-@Cerebral({
-    rowViewDimensions: ['rowViewDimensions'],
-    rowData: ['rowData'],
-    charWidth: ['charWidth'],
-    selectionLayer: ['selectionLayer'],
-    searchLayers: ['searchLayers'],
-    cutsiteLabelSelectionLayer: ['cutsiteLabelSelectionLayer'],
-    annotationHeight: ['annotationHeight'],
-    tickSpacing: ['tickSpacing'],
-    spaceBetweenAnnotations: ['spaceBetweenAnnotations'],
-    showFeatures: ['showFeatures'],
-    showTranslations: ['showTranslations'],
-    showParts: ['showParts'],
-    showOrfs: ['showOrfs'],
-    showAxis: ['showAxis'],
-    showCutsites: ['showCutsites'],
-    showReverseSequence: ['showReverseSequence'],
-    caretPosition: ['caretPosition'],
-    sequenceLength: ['sequenceLength'],
-    bpsPerRow: ['bpsPerRow'],
-    uppercase: ['uppercase']
-})
-
-class RowView extends React.Component {
-    getNearestCursorPositionToMouseEvent(event, callback) {
+export default class RowView extends React.Component {
+    getNearestCursorPositionToMouseEvent(rowData, event, callback) {
+        var charWidth = defaultCharWidth;
         var rowNotFound = true;
-        var visibleRowsContainer = this.refs.InfiniteScroller.getVisibleRowsContainerDomNode();
+        var visibleRowsContainer = this.InfiniteScroller.items;
         //loop through all the rendered rows to see if the click event lands in one of them
         var nearestBP = 0;
-        for (var relativeRowNumber = 0; relativeRowNumber < visibleRowsContainer.childNodes.length; relativeRowNumber++) {
-            var rowDomNode = visibleRowsContainer.childNodes[relativeRowNumber];
-            var boundingRowRect = rowDomNode.getBoundingClientRect();
-            // console.log('boundingRowRect.top', JSON.stringify(boundingRowRect.top,null,4));
-            // console.log('boundingRowRect.height', JSON.stringify(boundingRowRect.height,null,4));
-            if (event.clientY > boundingRowRect.top && event.clientY < boundingRowRect.top + boundingRowRect.height) {
-                //then the click is falls within this row
-                // console.log('HGGGG');
-                rowNotFound = false;
-                var rowNumber = this.refs.InfiniteScroller.state.visibleRows[relativeRowNumber];
-                var row = this.props.rowData[rowNumber];
-                if (event.clientX - boundingRowRect.left < 0) {
-                    nearestBP = row.start;
-                } else {
-                    var clickXPositionRelativeToRowContainer = event.clientX - boundingRowRect.left;
-                    var numberOfBPsInFromRowStart = Math.floor((clickXPositionRelativeToRowContainer + this.props.charWidth / 2) / this.props.charWidth);
-                    nearestBP = numberOfBPsInFromRowStart + row.start;
-                    if (nearestBP > row.end + 1) {
-                        nearestBP = row.end + 1;
-                    }
-                }
-                break; //break the for loop early because we found the row the click event landed in
-            }
-        }
+        some(visibleRowsContainer.childNodes, function (rowDomNode) {
+          var boundingRowRect = rowDomNode.getBoundingClientRect();
+          if (event.clientY > boundingRowRect.top && event.clientY < boundingRowRect.top + boundingRowRect.height) {
+              //then the click is falls within this row
+              rowNotFound = false;
+              var row = rowData[Number(rowDomNode.getAttribute('data-row-number'))];
+              if (event.clientX - boundingRowRect.left < 0) {
+                  nearestBP = row.start;
+              } else {
+                  var clickXPositionRelativeToRowContainer = event.clientX - boundingRowRect.left;
+                  var numberOfBPsInFromRowStart = Math.floor((clickXPositionRelativeToRowContainer + charWidth / 2) / charWidth);
+                  nearestBP = numberOfBPsInFromRowStart + row.start;
+                  if (nearestBP > row.end + 1) {
+                      nearestBP = row.end + 1;
+                  }
+              }
+              return true //break the loop early because we found the row the click event landed in
+          }
+        })
         if (rowNotFound) {
-            console.warn('was not able to find the correct row');
-            //return the last bp index in the rendered rows
-            var lastOfRenderedRowsNumber = this.refs.InfiniteScroller.state.visibleRows[this.refs.InfiniteScroller.state.visibleRows.length - 1];
-            var lastOfRenderedRows = this.props.rowData[lastOfRenderedRowsNumber];
-            nearestBP = lastOfRenderedRows.end
+            var {top, bottom} = visibleRowsContainer.getBoundingClientRect()
+            var numbers = [top,bottom]
+            var target = event.clientY
+            var topOrBottom = numbers.map(function(value, index) {
+              return [Math.abs(value - target), index];
+            }).sort().map(function(value) {
+              return numbers[value[1]];
+            })[0];
+            var rowDomNode
+            if (topOrBottom === top) {
+              rowDomNode = visibleRowsContainer.childNodes[0]
+            } else {
+              rowDomNode = visibleRowsContainer.childNodes[visibleRowsContainer.childNodes.length-1]
+            }
+            if (rowDomNode) {
+              var row = rowData[Number(rowDomNode.getAttribute('data-row-number'))];
+              //return the last bp index in the rendered rows
+              nearestBP = row.end
+            } else {
+              nearestBP = 0
+            }
         }
         callback({
             shiftHeld: event.shiftKey,
             nearestBP,
-            caretGrabbed: event.target.className === "cursor"
+            caretGrabbed
         });
-    }
-
-    resize() {
-        if (this.refs.rowView) {
-            this.props.signals.resizeRowView({
-                rootWidth: this.refs.rowView.clientWidth,
-                rootHeight: this.refs.rowView.clientHeight
-            });
-        }
-    }
-
-    componentDidMount() {
-        this.resize();
-        window.addEventListener('resize', this.resize.bind(this));
     }
 
     render() {
         var {
-            rowViewDimensions,
-            rowData, 
-            rowToJumpTo, 
-            charWidth,
-            selectionLayer,
-            searchLayers,
-            cutsiteLabelSelectionLayer,
-            annotationHeight,
-            tickSpacing,
-            spaceBetweenAnnotations,
-            showFeatures,
-            showTranslations,
-            showParts,
-            showOrfs,
-            showAxis,
-            showCutsites,
-            showReverseSequence,
-            caretPosition,
-            sequenceLength,
+            sequenceData = {},
+            editorDragged = noop,
+            editorDragStarted = noop,
+            editorClicked = noop,
+            editorDragStopped = noop,
+            width=defaultContainerWidth,
+            marginWidth=defaultMarginWidth,
+            height=400,
+            signals,
             bpsPerRow,
-            uppercase,
-            signals
+            rowData
         } = this.props;
 
-        function renderRows(rowNumber) {
-            if (rowData[rowNumber]) {
-                return (<RowItem
-                    charWidth={charWidth}
-                    selectionLayer={selectionLayer}
-                    searchLayers={searchLayers}
-                    cutsiteLabelSelectionLayer={cutsiteLabelSelectionLayer}
-                    annotationHeight={annotationHeight}
-                    tickSpacing={tickSpacing}
-                    spaceBetweenAnnotations={spaceBetweenAnnotations}
-                    showFeatures={showFeatures}
-                    showTranslations={showTranslations}
-                    showParts={showParts}
-                    showOrfs={showOrfs}
-                    showAxis={showAxis}
-                    showCutsites={showCutsites}
-                    showReverseSequence={showReverseSequence}
-                    caretPosition={caretPosition}
-                    sequenceLength={sequenceLength}
-                    bpsPerRow={bpsPerRow}
-                    signals={signals}
-                    key={rowNumber}
-                    uppercase={uppercase}
-                    row={rowData[rowNumber]} 
-                    />);
-            } else {
-                return null
-            }
+        var renderItem = (index,key) =>{
+          if (rowData[index]) {
+              return (
+                  <div data-row-number={index} key={key}>
+                      <div className={'veRowItemSpacer'}/>
+                      <RowItem {
+                              ...{
+                                  ...rest,
+                                  sequenceLength: sequenceData.sequence.length,
+                                  width: width - marginWidth,
+                                  bpsPerRow,
+                              }
+                          }
+                          row={rowData[index]}
+                          />
+                  </div>
+              );
+          } else {
+              return null
+          }
         }
 
         return (
             <Draggable
-            bounds={{top: 0, left: 0, right: 0, bottom: 0}}
-            onDrag={(event) => {
-                this.getNearestCursorPositionToMouseEvent(event, signals.editorDragged)}   
-            }
-            onStart={(event) => {
-                this.getNearestCursorPositionToMouseEvent(event, signals.editorDragStarted)}   
-            }
-            onStop={signals.editorDragStopped}
-            >
-              <div
-                ref="rowView"
-                className={styles.rowView}
-                onClick={(event) => {
-                    this.getNearestCursorPositionToMouseEvent(event, signals.editorClicked)}   
+                bounds={{top: 0, left: 0, right: 0, bottom: 0}}
+                onDrag={(event) => {
+                    this.getNearestCursorPositionToMouseEvent(event, sequenceLength, signals.editorDragged)}
                 }
+                onStart={(event) => {
+                    this.getNearestCursorPositionToMouseEvent(event, sequenceLength, signals.editorDragStarted)}
+                }
+                onStop={signals.editorDragStopped}
                 >
                 <div
-                    ref={'sequenceDiv'}
-                    averageElementHeight={100}
-                    containerHeight={rowViewDimensions.height}
-                    renderRow={renderRows}
-                    totalNumberOfRows={rowData.length}
-                    rowToJumpTo={rowToJumpTo}
-                    /> 
-              </div>
+                    ref={(ref) => this.rowViewComp = ref}
+                    className="veRowView"
+                    style={{
+                        overflowY: "auto",
+                        overflowX: "visible",
+                        height,
+                        marginLeft: marginWidth/2,
+                    }}
+                    onClick={(event) => {
+                        this.getNearestCursorPositionToMouseEvent(rowData, event, editorClicked)}
+                    }
+                    >
+
+                </div>
             </Draggable>
         );
     }
 }
 
-module.exports = RowView;
+function getBpsPerRow({
+    charWidth=defaultCharWidth,
+    width=defaultContainerWidth,
+    marginWidth=defaultMarginWidth
+}) {
+    return Math.floor((width-marginWidth)/charWidth)
+}
+
+function itemSizeEstimator(index, cache) {
+  if (cache[index+1]) {
+    return cache[index+1]
+  }
+  if (cache[index-1]) {
+    return cache[index-1]
+  }
+  return 100
+}
