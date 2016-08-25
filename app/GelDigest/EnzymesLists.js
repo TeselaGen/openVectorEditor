5519017cb953fbb4d34e99b936695945209caddd
Elena Aravina <perecmerec@gmail.com>
2016-08-25 13:40:40 -0700

    three section of dialog are set up

---
+++ app/GelDigest/EnzymesLists.js
@@ -0,0 +1,98 @@
+import React, {PropTypes} from 'react';
+import {Decorator as Cerebral} from 'cerebral-view-react';
+
+import List from 'material-ui/lib/lists/list';
+import ListItem from 'material-ui/lib/lists/list-item';
+import Checkbox from 'material-ui/lib/checkbox';
+import RaisedButton from 'material-ui/lib/raised-button';
+
+
+const DropDownMenu = require('material-ui/lib/drop-down-menu');
+
+@Cerebral({
+    // commonEnzymes: ['commonEnzymes'],
+    // // REBASEEnzymes: ['REBASEEnzymes'],
+    // berkeleyBBEnzymes: ['berkeleyBBEnzymes'],
+    // MITBBEnzymes: ['MITBBEnzymes'],
+    // fastDigestEnzymes: ['fastDigestEnzymes'],
+    // currentEnzymesList: ['currentEnzymesList'],
+    // currentUserEnzymesList: ['currentUserEnzymesList'],
+    // addEnzymeButtonValue: ['addEnzymeButtonValue'],
+    // addAllEnzymesButtonValue: ['addAllEnzymesButtonValue'],
+})
+
+export default class EnzymesLists extends React.Component {
+    constructor(props) {
+        super(props);
+        // this.props.signals.chooseEnzymeList({selectedList: this.props.commonEnzymes});
+        this.state = {
+            value: 1,
+        };
+    }
+
+    // handleChange = (event, index, value) => {
+    //     this.setState({value: value});
+    //     switch (value.text) {
+    //         case 'REBASE':
+    //             /**/
+    //             break;
+    //         case 'Berkeley BioBricks':
+    //             this.props.signals.chooseEnzymeList({selectedList: this.props.berkeleyBBEnzymes});
+    //             break;
+    //         case 'MIT BioBricks':
+    //             this.props.signals.chooseEnzymeList({selectedList: this.props.MITBBEnzymes});
+    //             break;
+    //         case 'Fermentas Fast Digest':
+    //             this.props.signals.chooseEnzymeList({selectedList: this.props.fastDigestEnzymes});
+    //             break;
+    //         default:
+    //             this.props.signals.chooseEnzymeList({selectedList: this.props.commonEnzymes});
+    //     }
+    // };
+
+    // isChecked = (enzyme) => {
+    //     return (this.props.currentUserEnzymesList.indexOf(enzyme) >= 0);
+    // };
+
+    render() {
+        var {
+            currentEnzymesList,
+            currentUserEnzymesList,
+            addEnzymeButtonValue,
+            addAllEnzymesButtonValue,
+            signals,
+        } = this.props;
+
+        let menuItems = [
+            { payload: '1', text: 'Common' },
+            { payload: '2', text: 'REBASE' },
+            { payload: '3', text: 'Berkeley BioBricks' },
+            { payload: '4', text: 'MIT BioBricks' },
+            { payload: '5', text: 'Fermentas Fast Digest' },
+        ];
+
+        return (
+            <div>
+                <DropDownMenu
+
+                    menuItems={menuItems}
+                    style={{backgroundColor: "#311B92"}}
+                    underlineStyle={{opacity: 0}}
+                    iconStyle={{color: "#000000"}}
+                    labelStyle={{fontWeight: 650, fontSize: 17, color: "#FFFFFF"}}
+                />
+                <br />
+                <List
+                        >
+
+                </List>
+                <br />
+                <RaisedButton
+                    label="Add all"
+                    secondary={true}
+
+                />
+            </div>
+        );
+    }
+}
