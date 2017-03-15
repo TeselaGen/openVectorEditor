f520e4bc115155e597437d4da01b5753d54fceaa
salafrance <sea.car.flan@gmail.com>
2017-03-15 15:36:01 -0700

    making empty sequence a little happier

--- app/CircularView/Caret.js
+++ app/CircularView/Caret.js
@@ -1,4 +1,4 @@
-import isNumber from 'lodash/lang/isNumber';
+// import isNumber from 'lodash/lang/isNumber';
 import getRangeAngles from 've-range-utils/getRangeAngles';
 import PositionAnnotationOnCircle from './PositionAnnotationOnCircle';
 import React, { PropTypes } from 'react';
@@ -9,25 +9,32 @@ export const draggableClassNames = ['selectionStart', 'selectionEnd', 'caretSvg'
 
 export default function Caret ({caretPosition, sequenceLength, className, innerRadius, outerRadius}) {
     var {startAngle, endAngle} = getRangeAngles({start: caretPosition, end:caretPosition}, sequenceLength)
-    if (!isNumber(startAngle)) {
-        console.error("we've got a problem!")
+    
+    // check for NaN or inf
+    if(startAngle != startAngle || startAngle === Infinity) {
+        startAngle = 0;
     }
+    if(endAngle != endAngle || endAngle === Infinity) {
+        endAngle = 0;
+    } // do we want to use zero?
+
     return (
         <PositionAnnotationOnCircle
-            key='caret'
-            className={className}
-            sAngle={ startAngle }
-            eAngle={ endAngle }
-            height={ 0 }>
+            key = 'caret'
+            className = { className }
+            sAngle = { startAngle }
+            eAngle = { endAngle }
+            height = { 0 }
+            >
             <line
-                className={className}
-                strokeWidth='1px'
-                style={ { opacity: 9, zIndex: 100,  cursor: "ew-resize",} }//tnr: the classname needs to be cursor here!
-                x1={0}
-                y1={-innerRadius}
-                x2={0}
-                y2={-outerRadius}
-                stroke="blue" 
+                className = { className }
+                strokeWidth = '1px'
+                style = {{ opacity: 9, zIndex: 100,  cursor: "ew-resize" }}
+                x1 = { 0 }
+                y1 = { -innerRadius }
+                x2 = { 0 }
+                y2 = { -outerRadius }
+                stroke = "blue" 
                 />
         </PositionAnnotationOnCircle>
     )
