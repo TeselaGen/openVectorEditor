a771ee40a8e1544b621372d957b7c40c4b99e3bb
salafrance <sea.car.flan@gmail.com>
2017-04-27 15:34:59 -0700

    loading and displaying partial sequencedata, can't upload to server yet

--- app/cerebral/actions/overwriteSequenceData.js
+++ app/cerebral/actions/overwriteSequenceData.js
@@ -6,14 +6,24 @@ function overwriteSequenceData({input, state, output}) {
     var { newSequenceData } = input;
     var newSeq = newSequenceData.sequence;
     var newName = newSequenceData.name;
-    console.log(newSeq)
-    console.log(newName)
+    var newFeatures = newSequenceData.features;
+    // do schemaConvert's job again
+    var feature;
+    for(var f = 0; f < newFeatures.length; f++) {
+        feature = newFeatures[f];
+        feature.start = feature.locations[0].genbankStart;
+        feature.end = feature.locations[0].end;
+        feature.id = "feature_" + f;
+    }
     
+    // keep in mind there may be no features and no name / labels
     // clear previous state and delete sequence and features
     state.set('caretPosition', 0);
-    state.set('history', [])
+    state.set('history', []) // need to put this action in so we know to save
     state.set(['sequenceData', 'name'], newName);
-
+    state.set(['sequenceData', 'sequence'], newSeq);
+    state.set(['sequenceData', 'features'], newFeatures);
+    // state.set('sequenceData', {newSequenceData}); // do we need schemaconvert here?
 }
 
 export default overwriteSequenceData;
