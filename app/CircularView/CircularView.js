23deb76f0e00dfac8fb36da002507d76d7db7e0c
ltspringthorpe <lrteeter@gmail.com>
2017-05-23 13:17:57 -0700

    circular view double-click working

--- app/CircularView/CircularView.js
+++ app/CircularView/CircularView.js
@@ -50,10 +50,12 @@ function toDegrees(radians) {
 
 export default class CircularView extends React.Component {
     getNearestCursorPositionToMouseEvent(event, sequenceLength, callback) {
-        // debugger
+
         if (event.target.nodeName.toLowerCase() === "path" ||
             event.target.nodeName.toLowerCase() === "circle" ||
-            event.target.className === "cutsiteLabel") {
+            event.target.className === "cutsiteLabel" ||
+            event.target.className.baseVal === "velabelText veCircularViewLabelText clickable veCutsiteLabel" ||
+            event.target.className.baseVal === "velabelText veCircularViewLabelText clickable veFeatureLabel") {
             return;
         }
 
