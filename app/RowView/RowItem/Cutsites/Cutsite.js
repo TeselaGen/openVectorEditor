92976b1c1e8e09e3a69bc695c113ca73798a9d78
salafrance <sea.car.flan@gmail.com>
2017-05-24 15:01:07 -0700

    just making some typo and styling changes

--- app/RowView/RowItem/Cutsites/Cutsite.js
+++ app/RowView/RowItem/Cutsites/Cutsite.js
@@ -22,24 +22,25 @@ var Cutsite = React.createClass({
             height: height
         });
         return (
-            <g
-                // onClick={this.props.onClick}
-                >
+            <g>
                 <polyline
-                    transform={forward ? null : "translate("+width+",2.5) scale(-1,1) "} //TODO: this 2.5 shouldn't be hardcoded. it is in there to make the annotation slightly smaller
-                    points={points}
-                    strokeWidth="1"
-                    stroke={this.props.color}
-                    fillOpacity={0.4}
-                    fill={this.props.color || 'orange'}>
+                    transform = { forward ? null : "translate(" + width + ",2.5) scale(-1,1) " } 
+                    // TODO: this 2.5 shouldn't be hardcoded. 
+                    // it is in there to make the annotation slightly smaller
+                    points = { points }
+                    strokeWidth = "1"
+                    stroke = { this.props.color }
+                    fillOpacity = { 0.4 }
+                    fill = { this.props.color || 'orange' }
+                    >
                 </polyline>
                 <text
-                    transform={"translate("+width/2+",0)"}
-                    x="0"
-                    y="13"
-                    style={{textAnchor: "middle"}}
+                    transform = { "translate(" + width/2 + ",0)" }
+                    x = "0"
+                    y = "13"
+                    style = {{ textAnchor : "middle" }}
                     >
-                    {name}
+                    { name }
                 </text>
             </g>
         );
