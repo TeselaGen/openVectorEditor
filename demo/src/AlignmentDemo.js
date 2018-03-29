dfd8487f4df07bbb47e0d393cd9000e4a7b2a29b
mmatena <michael.matena@teselagen.com>
2018-03-29 11:20:43 -0700

    First pass at what I need for massively parallel part creation

--- demo/src/AlignmentDemo.js
+++ demo/src/AlignmentDemo.js
@@ -1,35 +1,78 @@
 import React from "react";
 import { connect } from "react-redux";
-import { times } from "lodash";
+import { times, get } from "lodash";
 import store from "./store";
 import alignmentRunData from "./exampleData/alignmentRunData.json";
-import { addAlignment, AlignmentView } from "../../src/";
+import { addAlignment, AlignmentView, updateEditor } from "../../src/";
 import { selectionLayerUpdate } from "../../src/redux/selectionLayer";
 import {
   getCombinedActions,
   fakeActionOverrides
 } from "../../src/withEditorProps/index";
 import { caretPositionUpdate } from "../../src/redux/caretPosition";
+import { upsertPart } from "../../src/redux/sequenceData";
 // import { MenuItem } from "@blueprintjs/core";
 
+// Use the line below because using the full 30 sequences murders Redux dev tools.
+alignmentRunData.alignmentTracks = alignmentRunData.alignmentTracks.slice(0, 5);
+
 const basicActions = { selectionLayerUpdate, caretPositionUpdate };
-export default connect(null, dispatch => {
-  return {
-    ...getCombinedActions(
-      times(alignmentRunData.alignmentTracks.length, i => "alignmentView" + i),
-      basicActions,
-      fakeActionOverrides,
-      dispatch
-    )
-  };
-})(
+export default connect(
+  state => {
+    // Use the state of the first alignment state to get the selection
+    // data.
+    const alignmentState = state.VectorEditor.alignmentView0;
+    return {
+      selectionLayer: get(alignmentState, "selectionLayer", {})
+    };
+  },
+  dispatch => {
+    return {
+      ...getCombinedActions(
+        times(
+          alignmentRunData.alignmentTracks.length,
+          i => "alignmentView" + i
+        ),
+        basicActions,
+        fakeActionOverrides,
+        dispatch
+      )
+    };
+  }
+)(
   class StandaloneDemo extends React.Component {
     componentDidMount() {
-      addAlignment(store, "MppViewer", alignmentRunData);
-      // addAlignment(store, "MppViewer", {...alignmentRunData, alignmentTracks: alignmentRunData.alignmentTracks.slice(0,10)});
+      addAlignment(store, "MppViewer", { ...alignmentRunData });
+
+      alignmentRunData.alignmentTracks.forEach((at, i) => {
+        // We need to do this or else the sequence data will be
+        // updated with some default sequence data when we select
+        // stuff.
+        updateEditor(store, "alignmentView" + i, {
+          readOnly: true,
+          sequenceData: at.alignmentData,
+          annotationVisibility: {
+            features: false,
+            translations: false,
+            parts: true,
+            orfs: false,
+            orfTranslations: false,
+            axis: false,
+            cutsites: false,
+            primers: false,
+            reverseSequence: false,
+            lineageLines: false,
+            axisNumbers: false
+          }
+        });
+      });
     }
     render() {
-      const { selectionLayerUpdate, caretPositionUpdate } = this.props;
+      const {
+        selectionLayerUpdate,
+        caretPositionUpdate,
+        selectionLayer
+      } = this.props;
       return (
         <AlignmentView
           {...{
@@ -40,28 +83,52 @@ export default connect(null, dispatch => {
             },
             minimapLaneHeight: 10,
             minimapLaneSpacing: 2,
-            // hideBottomBar: true,
             linearViewOptions: ({ index, alignmentVisibilityToolOptions }) => {
               const toReturn = {
                 tickSpacing: 10,
+                ignoreYWhenSelecting: true,
                 rightClickOverrides: {
-                  selectionLayerRightClicked: (items, opts, props) => {
-                    if (0) props(props ^ props);
-                    return [{ text: "Create Diversity Region" }];
+                  selectionLayerRightClicked: () => {
+                    return [
+                      {
+                        text: "Create Diversity Region",
+                        onClick: () => {
+                          store.dispatch(
+                            upsertPart(
+                              {
+                                name: "Diversity Region",
+                                strand: 1,
+                                forward: true,
+                                start: selectionLayer.start,
+                                end: selectionLayer.end
+                              },
+                              { editorName: "alignmentView0" }
+                            )
+                          );
+                        }
+                      }
+                    ];
+                  },
+                  partRightClicked: (items, { annotation }, props) => {
+                    return [
+                      {
+                        text: "Delete Diversity Region",
+                        onClick: () => props.deletePart(annotation)
+                      }
+                    ];
                   }
-                  // partRightClicked: () => {}
                 },
                 selectionLayerUpdate,
-                caretPositionUpdate
+                caretPositionUpdate,
+                linearViewAnnotationVisibilityOverrides: {
+                  ...alignmentVisibilityToolOptions.alignmentAnnotationVisibility,
+                  parts: true
+                }
               };
               if (index === alignmentRunData.alignmentTracks.length - 1) {
-                Object.assign(toReturn, {
-                  linearViewAnnotationVisibilityOverrides: {
-                    ...alignmentVisibilityToolOptions.alignmentAnnotationVisibility,
-                    axis: true
-                  }
-                });
+                toReturn.linearViewAnnotationVisibilityOverrides.axis = true;
               }
+
               return toReturn;
             }
           }}
