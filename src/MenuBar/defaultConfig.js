5a7f7a2fc91b0908ea19335fd15667885327a31c
Juan Pablo Carzolio <jpcarzolio@gmail.com>
2018-05-11 17:38:59 -0300

    Implement MenuBar (WIP)

--- src/MenuBar/defaultConfig.js
+++ src/MenuBar/defaultConfig.js
@@ -1,279 +1,243 @@
 /* eslint-disable */
+import { addMenuHotkeys, addMenuHandlers } from "teselagen-react-components";
+import hotkeys from "./menuHotkeys";
+import handlers from "./handlers";
 
+// TODO wrap this in a function and update dynamic stuff, e.g. checked, disabled,
+// etc. as needed
 const defaultConfig = [
   {
     text: "File",
-    menu: [
+    submenu: [
+      { cmd: "newSequence" },
+      { cmd: "renameSequence" },
+      { cmd: "saveSequence" },
+      { cmd: "deleteSequence" },
+      { cmd: "setReadOnly", disabled: true },
+      { cmd: "duplicate" },
+
+      { divider: "" },
+
+      { cmd: "importSequences", text: "Import Sequences" },
+      { cmd: "exportToFile" },
+
+      { divider: "" },
+
       {
-        text: "New Sequence"
+        cmd: "print",
+        submenu: [{ cmd: "circularView" }, { cmd: "linearView" }]
       },
-      {
-        text: "Rename Sequence"
-      },
-      {
-        text: "Save Sequence",
-        hotKey: "modKey" + "+S"
-      },
-      {
-        text: "Delete Sequence"
-      },
-      {
-        text: "Set Read Only",
-        disabled: true
-      },
-      {
-        text: "Duplicate"
-      },
-      {
-        text: "Import Sequence(s)"
-      },
-      {
-        text: "Export to File"
-      },
-      {
-        text: "Print",
-        hotKey: "modKey" + "+P",
-        menu: [
-          {
-            text: "Circular View"
-          },
-          {
-            text: "Linear View"
-          }
-        ]
-      },
-      {
-        text: "View revision history"
-      },
-      {
-        text: "Properties"
-      }
+      { cmd: "viewRevisionHistory" },
+      { cmd: "properties" }
     ]
   },
   {
     text: "Edit",
-    menu: [
-      { text: "Cut" },
-      { text: "Copy" },
+    submenu: [
+      { cmd: "cut", icon: "cut" },
+      { cmd: "copy", icon: "duplicate" },
       {
-        text: "Copy Options",
-        menu: [
+        cmd: "copyOptions",
+        submenu: [
           {
-            text: "Include Features",
+            cmd: "includeFeatures",
             checked: true
           },
           {
-            text: "Include Partial Features",
+            cmd: "includePartialFeatures",
             checked: false
           },
           {
-            text: "Include Parts",
+            cmd: "includeParts",
             checked: true
           },
           {
-            text: "Include Partial Parts",
+            cmd: "includePartialParts",
             checked: false
           }
         ]
       },
-      {
-        text: "Paste",
-        hotKey: "modKey" + "+V"
-      },
-      {
-        disabled: true,
-        text: "Undo",
-        hotKey: "modKey" + "+Z"
-      },
-      {
-        disabled: true,
-        text: "Redo",
-        hotKey: "modKey" + "+Shift+Z"
-      },
-      {
-        text: "Find...",
-        hotKey: "modKey" + "+F"
-      },
-      {
-        text: "Go to...",
-        hotKey: ""
-      },
-      {
-        text: "Select..."
-      },
-      {
-        text: "Select All",
-        hotKey: "modKey" + "+A"
-      },
-      {
-        text: "Select Inverse",
-        hotKey: "modKey" + "+I"
-      },
-      {
-        text: "Complement Selection"
-      },
-      {
-        text: "Complement Entire Sequence"
-      },
-      {
-        text: "Reverse Complement Selection",
-        hotKey: "modKey" + "+E"
-      },
-      {
-        text: "Reverse Complement Entire Sequence",
-        hotKey: "modKey" + "+Shift+E"
-      },
-      {
-        text: "Rotate to Caret Position",
-        hotKey: "modKey" + "+B"
-      },
-      {
-        text: "New Feature",
-        hotKey: "modKey" + "+K"
-      },
-      {
-        text: "New Part",
-        hotKey: "modKey" + "+L"
-      }
+      { cmd: "paste", icon: "clipboard" },
+      { cmd: "undo", disabled: true, icon: "undo" },
+      { cmd: "redo", disabled: true, icon: "redo" },
+
+      { divider: "" },
+
+      { cmd: "find", icon: "search" },
+      { cmd: "goTo", icon: "locate", text: "Go To..." },
+
+      { divider: "" },
+
+      { cmd: "select" },
+      { cmd: "selectAll" },
+      { cmd: "selectInverse" },
+      { cmd: "complementSelection" },
+      { cmd: "complementEntireSequence" },
+      { cmd: "reverseComplementSelection" },
+      { cmd: "reverseComplementEntireSequence" },
+      { cmd: "rotatetoCaretPosition" },
+
+      { divider: "" },
+
+      { cmd: "newFeature" },
+      { cmd: "newPart" }
     ]
   },
   {
     text: "View",
-    menu: [
+    submenu: [
+      { cmd: "circular", checked: true },
+      { cmd: "linear", checked: false },
+      { cmd: "mapCaret", checked: true },
+      { cmd: "features", checked: true },
       {
-        text: "Circular",
-        checked: true
-      },
-      {
-        text: "Linear",
-        checked: false
-      },
-      {
-        text: "Map Caret",
-        checked: true
-      },
-      {
-        text: "Features",
-        checked: true
-      },
-      {
-        text: "Feature Types",
-        itemId: "featureTypes"
+        // TODO preprocess this as needed
+        cmd: "featureTypes",
+        itemId: "featureTypes",
         //submenu of checklist of all feature types here
+        submenu: [{ text: "TO DO...", disabled: true }]
       },
+      { cmd: "parts", checked: true },
+      { cmd: "cutSites", checked: true },
       {
-        text: "Parts",
-        checked: true
-      },
-      {
-        text: "Cut Sites"
-      },
-      {
+        cmd: "ORFs",
         text: "ORFs",
-        menu: [
+        submenu: [
           {
+            cmd: "ORFs_allFrames",
             text: "All Frames",
             frameNumber: "all"
           },
           {
-            text: "Frame 1",
-            frameNumber: 1
+            cmd: "ORFs_frame1",
+            text: "Frame 1"
+            // frameNumber: 1 // TODO ?
           },
           {
-            text: "Frame 2",
-            frameNumber: 2
+            cmd: "ORFs_frame2",
+            text: "Frame 2"
+            // frameNumber: 2 // TODO ?
           },
           {
-            text: "Frame 3",
-            frameNumber: 3
+            cmd: "ORFs_frame3",
+            text: "Frame 3"
+            // frameNumber: 3 // TODO ?
+          }
+        ]
+      },
+      { cmd: "complementary", checked: true },
+      { cmd: "spaces", checked: true },
+      {
+        cmd: "sequenceAA",
+        submenu: [
+          {
+            cmd: "sequenceAA_allFrames",
+            text: "All Frames"
+            // frameNumber: "all" // TODO ?
+          },
+          {
+            cmd: "sequenceAA_frame1",
+            text: "Frame 1"
+            // frameNumber: 1 // TODO ?
+          },
+          {
+            cmd: "sequenceAA_frame2",
+            text: "Frame 2"
+            // frameNumber: 2 // TODO ?
+          },
+          {
+            cmd: "sequenceAA_frame3",
+            text: "Frame 3"
+            // frameNumber: 3 // TODO ?
           }
         ]
       },
       {
-        text: "Complementary",
-        checked: true
-      },
-      {
-        text: "Spaces",
-        checked: true
-      },
-      {
-        text: "Sequence AA",
-        menu: [
+        cmd: "revcomAA",
+        submenu: [
           {
-            text: "All Frames",
-            frameNumber: "all"
+            cmd: "revcomAA_allFrames",
+            text: "All Frames"
+            // frameNumber: "all" // TODO ?
           },
           {
-            text: "Frame 1",
-            frameNumber: 1
+            cmd: "revcomAA_frame1",
+            text: "Frame 1"
+            // frameNumber: 1 // TODO ?
           },
           {
-            text: "Frame 2",
-            frameNumber: 2
+            cmd: "revcomAA_frame2",
+            text: "Frame 2"
+            // frameNumber: 2 // TODO ?
           },
           {
-            text: "Frame 3",
-            frameNumber: 3
+            cmd: "revcomAA_frame3",
+            text: "Frame 3"
+            // frameNumber: 3 // TODO ?
           }
         ]
       },
       {
-        text: "Revcom AA",
-        menu: [
-          {
-            text: "All Frames",
-            frameNumber: "all"
-          },
-          {
-            text: "Frame 1",
-            frameNumber: 1
-          },
-          {
-            text: "Frame 2",
-            frameNumber: 2
-          },
-          {
-            text: "Frame 3",
-            frameNumber: 3
-          }
-        ]
-      },
-      {
-        text: "Feature Labels",
+        cmd: "featureLabels",
         checked: true
       },
       {
-        text: "Part Labels",
+        cmd: "partLabels",
         checked: true
       },
       {
-        text: "Cut Site Labels",
+        cmd: "cutSiteLabels",
         checked: true
       },
+
+      { divider: "" },
+
       {
-        hideOnClick: false,
-        text: "Zoom In",
-        hotKey: "+"
+        cmd: "zoomIn",
+        shouldDismissPopover: false,
+        icon: "zoom-in"
       },
       {
-        hideOnClick: false,
-        text: "Zoom Out",
-        hotKey: "-"
+        cmd: "zoomOut",
+        shouldDismissPopover: false,
+        icon: "zoom-out"
       }
     ]
   },
   {
     text: "Tools",
-    menu: [
-      {
-        text: "Restriction Enzymes Manager"
-      },
-      {
-        text: "Simulate Digestion"
-      }
+    submenu: [
+      { cmd: "restrictionEnzymesManager" },
+      { cmd: "simulateDigestion" }
     ]
   }
 ];
 
-export default defaultConfig;
+// TODO remove - copied from TRC
+// Recursively walk the given menu and run each item through func
+function walkMenu(menuDef, func) {
+  if (menuDef instanceof Array) {
+    return menuDef.map(item => walkMenu(item, func));
+  }
+  const out = func(menuDef);
+  if (out.submenu) {
+    out.submenu = out.submenu.map(item => walkMenu(item, func));
+  }
+  return out;
+}
+
+// TODO move into TRC's createMenu or a new menu util
+const addMenuChecks = menu =>
+  walkMenu(menu, item => {
+    const out = { ...item };
+    if (out.checked !== undefined) {
+      out.icon = out.checked ? "small-tick" : "blank";
+    }
+    return out;
+  });
+
+let menu = addMenuChecks(defaultConfig);
+menu = addMenuHotkeys(menu, hotkeys);
+menu = addMenuHandlers(menu, handlers);
+
+export default menu;
