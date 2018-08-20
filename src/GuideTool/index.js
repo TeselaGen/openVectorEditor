b5e1ace66b885bdb47ecc8b0c6914d5978287c6a
Thomas Rich <tnrich@teselagen.com>
2018-08-20 14:23:18 -0700

    adding guide tool tab

---
+++ src/GuideTool/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class GuideTool extends React.Component {
+  render () {
+    // const {} = this.props
+    console.log('this.props:',this.props)
+    return (
+      <div>
+        I'm the guide tool
+      </div>
+    )
+  }
+}
