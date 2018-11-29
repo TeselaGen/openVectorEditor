5321fe98c410610cc801cf28ad756afb7541788e
tnrich <tnrich@teselagen.com>
2018-11-29 11:06:01 -0800

    updating tests

--- cypress/integration/menuBar.js
+++ cypress/integration/menuBar.js
@@ -2,6 +2,17 @@ describe("menuBar", function() {
   beforeEach(() => {
     cy.visit("");
   });
+  
+  it('should be able to select a range (10 - 20) via Edit > Select and have the range correctly selected', function() {
+      cy.get(".tg-menu-bar").contains("Edit").click()
+      cy.get(".tg-menu-bar-popover").contains("Select").click()
+      cy.get(`[label="From:"]`).clear().type("10")
+      cy.get(`[label="To:"]`).clear().type("20")
+      cy.get(".tg-min-width-dialog").contains("OK").click()
+      cy.get(".veStatusBarItem").contains("10 to 20").should("be.visible")
+  })
+
+
   it("menubar can be optionally displayed above or on the same line as the shortcuts", function() {
     cy.contains("showOptions").find("input").check({force: true})
     cy.contains("displayMenuBarAboveTools").find("input").check({force: true})
@@ -9,7 +20,8 @@ describe("menuBar", function() {
     cy.contains("displayMenuBarAboveTools").find("input").uncheck({force: true})
     cy.get(".veTools-displayMenuBarAboveTools").should("not.exist")
   });
-  it.only("select range, copy, cut works", function() {
+  it("select range, copy, cut works", function() {
+    cy.clock()
     cy.get('.tg-menu-bar').contains("Edit").click()
     cy.get('.tg-menu-bar-popover').contains("Select").click()
     cy.get(`[label="From:"]`).clear().type("10")
--- cypress/integration/rowView.js
+++ cypress/integration/rowView.js
@@ -5,6 +5,7 @@ describe("rowView", function() {
   it("can jump to end and start in row view using the buttons", function() {
     cy.get("[data-test=jumpToEndButton]").click();
     cy.get("[data-test=jumpToStartButton]").click();
+    cy.get("[data-test=jumpToEndButton]").click();
   });
   it('can click a feature multiple times and have the row view jump to the start and end of the feature', function() {
     cy.get(".veLabelText").contains("GFPuv").click()
