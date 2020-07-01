d5aeb964816a6480067a59dc664ac8c15089ef52
deltuh-vee <739curli@gmail.com>
2020-07-01 13:35:31 -0700

    Added selectors for filtering features

--- src/selectors/filteredFeaturesSelector.js
+++ src/selectors/filteredFeaturesSelector.js
@@ -2,12 +2,17 @@ import { createSelector } from "reselect";
 import { omitBy } from "lodash";
 import featuresSelector from "./featuresSelector";
 import featureTypesToHideSelector from "./featureTypesToHideSelector";
+import featureLengthsToHide from "./featureLengthsToHide";
 
 function filteredFeaturesSelector(features, featureTypesToHide) {
-  return omitBy(features, feat => {
-    if (featureTypesToHide[feat.type]) {
-      return true;
-    }
+  return omitBy(features, (feat) => {
+    const featLength = feat.end - feat.start;
+    const hideFeaturesByType = featureTypesToHide[feat.type];
+    const hideFeaturesByLength =
+      featureLengthsToHide.enabled &&
+      (featLength < featureLengthsToHide.min ||
+        featLength > featureLengthsToHide.max);
+    return hideFeaturesByLength || hideFeaturesByType;
   });
 }
 
