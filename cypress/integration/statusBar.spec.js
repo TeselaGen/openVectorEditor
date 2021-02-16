ed37add2f4d8e2d30f354d13114c5d01ebd15773
Thomas Rich <tnrich@thomass-mbp.lan>
2021-02-16 11:21:33 -0800

    fixing select linear test

--- cypress/integration/statusBar.spec.js
+++ cypress/integration/statusBar.spec.js
@@ -8,22 +8,15 @@ describe("statusBar", function () {
     cy.contains("Melting Temp: 62.69").click();
     cy.get(`[value="default"][checked]`);
   });
-  it("can change to linear mode via the status bar and get a warning that annotations will be truncated", function () {
-    cy.visit("");
-    cy.get(`[data-test="veStatusBar-circularity"]`)
-      .find("select")
-      .select("Linear");
-    cy.contains("Truncate Annotations").should("be.visible");
-  });
   it(`if viewing a linear sequence in the circular view, there should be a little warning 
-  cy.visit("");
   on the circular view telling the user that the sequence is linear`, () => {
+    cy.visit("");
     cy.contains(`[data-test="ve-warning-circular-to-linear"]`).should(
       "not.exist"
     );
     cy.get(`[data-test="veStatusBar-circularity"]`)
       .find("select")
-      .select("Linear");
+      .select("Linear", { force: true });
     cy.contains("Truncate Annotations").click();
     cy.get(`[data-test="ve-warning-circular-to-linear"]`);
   });
