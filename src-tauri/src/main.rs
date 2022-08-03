c1dc082bf5dcb87cddb808568250e0555a6b1782
Thomas Rich <tnrich@teselagen.com>
2022-08-03 16:58:26 -0700

    fiddling with tauri

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,25 @@
+
+
+#![cfg_attr(
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
+)]
+
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
+
+fn main() {
+
+  // here `"quit".to_string()` defines the menu item id, and the second parameter is the menu item label.
+  let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+  let close = CustomMenuItem::new("close".to_string(), "Close");
+  let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(close));
+  let menu = Menu::new()
+    .add_native_item(MenuItem::Copy)
+    .add_item(CustomMenuItem::new("hide", "Hide"))
+    .add_submenu(submenu);
+
+  tauri::Builder::default()
+    .menu(menu)
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
