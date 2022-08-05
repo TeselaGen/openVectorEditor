884a44e02ea2fe76faf946b6c041ff8841caad72
Thomas Rich <tnrich@teselagen.com>
2022-08-05 10:41:09 -0700

    migrating to recommended cypress defaults

--- cypress/e2e/tabs.spec.js
+++ cypress/e2e/tabs.spec.js
@@ -1,4 +1,4 @@
-describe("tabs", function() {
+describe("tabs", function () {
   beforeEach(() => {
     cy.visit("");
   });
@@ -10,35 +10,27 @@ describe("tabs", function() {
     cy.contains(".tg-editor-container", "sequenceLength: 5299");
   });
 
-  it("can open the new part window via hotkey and add a new part", function() {
+  it("can open the new part window via hotkey and add a new part", function () {
     if (Cypress.browser.isHeadless) return true; //stop early because this test fails currently in headless mode
     // cy.get('body').trigger('keydown', { keyCode: 114, which: 114 })
     cy.get("body").type("{meta}l");
     cy.contains("New Part");
 
-    cy.focused()
-      .clear()
-      .type("newPart1");
-    cy.get(`[label="Start:"]`)
-      .clear()
-      .type("10");
-    cy.get(`[label="End:"]`)
-      .clear()
-      .type("20");
+    cy.focused().clear().type("newPart1");
+    cy.get(`[label="Start:"]`).clear().type("10");
+    cy.get(`[label="End:"]`).clear().type("20");
     cy.get(`[data-test="savePart"]`).click();
-    cy.get(".veLabelText")
-      .contains("newPart1")
-      .click();
+    cy.get(".veLabelText").contains("newPart1").click();
     cy.get(".veStatusBar").contains(`10 to 20`);
   });
 
-  it("can switch between tabs", function() {
+  it("can switch between tabs", function () {
     cy.contains("Linear Map").click();
     cy.get(".veLinearView");
     cy.contains("Circular Map").click();
     cy.get(".veCircularView");
   });
-  it("can drag tabs", function() {
+  it("can drag tabs", function () {
     cy.dragBetween(".veTabLinearMap", ".veTabProperties");
     cy.get(`[data-test="ve-draggable-tabs1"] .veTabLinearMap`, {
       force: true
--- cypress/e2e/versionHistory.spec.js
+++ cypress/e2e/versionHistory.spec.js
@@ -1,14 +1,10 @@
-describe("versionHistory", function() {
+describe("versionHistory", function () {
   beforeEach(() => {
     cy.visit("");
   });
-  it("should be accessible from the demo", function() {
-    cy.get(".tg-menu-bar")
-      .contains("File")
-      .click();
-    cy.get(".bp3-menu-item")
-      .contains("Revision History")
-      .click();
+  it("should be accessible from the demo", function () {
+    cy.get(".tg-menu-bar").contains("File").click();
+    cy.get(".bp3-menu-item").contains("Revision History").click();
     cy.contains("Past Versions");
     cy.contains(".bp3-button", "Revert to Selected").should(
       "have.class",
