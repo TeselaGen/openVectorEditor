46d8a7105a2160a125100ffbfe63985b8993c412
Thomas Rich <tnrich@teselagen.com>
2022-11-07 14:03:25 -0800

    fixing a /lib requirement issue

--- src/helperComponents/withHover.js
+++ src/helperComponents/withHover.js
@@ -5,7 +5,7 @@ import React from "react";
 import { store } from "@risingstack/react-easy-state";
 import * as hoveredAnnotationActions from "../redux/hoveredAnnotation";
 import { withHandlers, branch } from "recompose";
-import { annotationTypes } from "ve-sequence-utils/lib";
+import { annotationTypes } from "ve-sequence-utils";
 
 export const HoveredIdContext = React.createContext({
   hoveredId: "" // default value
