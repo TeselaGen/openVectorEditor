c723952cfd350eede6f443846134e74285812d9c
Andrés Pérez Manríquez <apandresipm@gmail.com>
2022-12-28 15:24:14 -0300

    added missing test name

--- cypress/e2e/annotationExtraProps.spec.js
+++ cypress/e2e/annotationExtraProps.spec.js
@@ -1,5 +1,5 @@
 describe("annotation extra props", function () {
-  it("", () => {
+  it("should receive the extra annotation props", () => {
     cy.visit("http://localhost:3344/#/Editor?extraAnnotationPropsExample=true");
     cy.contains("Part 1 (digest)");
     cy.get(".partWithOverhangsIndicator");
