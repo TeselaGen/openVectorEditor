f81299604c23e48a38aed4ac9efabe960f18c4ae
Thomas Rich <tnrich@teselagen.com>
2023-06-09 10:12:01 -0700

    fixing buildWithDemo

--- vite.config.demo.js
+++ vite.config.demo.js
@@ -1,6 +1,6 @@
 // vite.config.js
 import { defineConfig } from "vite";
-import { config } from "./vite.config";
+import config from "./vite.config";
 config.build = {
   ...config.build,
   lib: undefined, //remove the lib build so we just get the demo build
