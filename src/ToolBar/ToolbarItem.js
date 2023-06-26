995d6c53530abfbfd453cbdd5b5b489c2e74f8d3
Thomas Rich <tnrich@teselagen.com>
2023-06-26 13:31:17 -0700

    tweaks

--- src/ToolBar/ToolbarItem.js
+++ src/ToolBar/ToolbarItem.js
@@ -14,6 +14,7 @@ import "./style.css";
 class ToolbarItem extends React.Component {
   toggleDropdown = ({ forceClose } = {}) => {
     const { toolName, isOpen } = this.props;
+
     this.props.openToolbarItemUpdate(isOpen || forceClose ? "" : toolName);
   };
 
